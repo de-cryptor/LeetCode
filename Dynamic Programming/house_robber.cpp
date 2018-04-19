bc1808ecb9f44a0cb71bad65b76dfc09e8e3a899
Unknown <bim2014001@iiita.ac.in>
2018-04-19 15:28:09 +0530

    House Robber [198]

---
+++ Dynamic Programming/house_robber.cpp
@@ -0,0 +1,27 @@
+/* 
+    @Author - Jatin Goel
+    @Institute - IIIT Allahabad
+    Hardwork definitely pays off. 
+    There is no substitute of hardwork.
+    There is no shortcut to success. 
+*/
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return nums[0];
+        int dp[n];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+    }
+};
--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 	2. Best Time to Buy and Sell Stock [121]
 	3. Climbing Stairs [70]
 	4. Maximum Subarray [53]
+	5. House Robber [198]
